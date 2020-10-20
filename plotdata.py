0a713bf910d44e809a83f0d9e8a27a8ebadb6012
Patricia Schady <pschady@gmail.com>
2020-10-20 23:26:44 +0100

    Changed pylab to matplotlib.pyplot

--- plotdata.py
+++ plotdata.py
@@ -1,4 +1,4 @@
-import pylab
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy import optimize
 from scipy.interpolate import interpn
@@ -11,22 +11,22 @@ Plotting data
 #------------------------------------------------------------------------
 def plotspec(spec1,spec2,spec3,spec4,n1=10,n2=15,n3=4,n4=10,lab1='NGC4258',lab2='NGC4639',lab3='Early type',lab4='Late type',fs=15,xmin=3500,xmax=9200,ymin=0,ymax=16):
     
-    pylab.plot(spec1[0,:],spec1[1,:]*n1/max(spec1[1,:]),label=lab1)
-    pylab.plot(spec2[0,:],spec2[1,:]*n2/max(spec2[1,:]),label=lab2)
-    pylab.plot(spec3[0,:],spec3[1,:]*n3/max(spec3[1,:]),label=lab3)
-    pylab.plot(spec4[0,:],spec4[1,:]*n4/max(spec4[1,:]),label=lab4)
-    pylab.xlabel("Wavelength (Angstrom)",size=20)
-    pylab.ylabel('Normalised flux',size=20)
-    pylab.xlim(xmin,xmax)
-    pylab.ylim(ymin,ymax)
-    pylab.legend(fontsize=fs)
+    plt.plot(spec1[0,:],spec1[1,:]*n1/max(spec1[1,:]),label=lab1)
+    plt.plot(spec2[0,:],spec2[1,:]*n2/max(spec2[1,:]),label=lab2)
+    plt.plot(spec3[0,:],spec3[1,:]*n3/max(spec3[1,:]),label=lab3)
+    plt.plot(spec4[0,:],spec4[1,:]*n4/max(spec4[1,:]),label=lab4)
+    plt.xlabel("Wavelength (Angstrom)",size=20)
+    plt.ylabel('Normalised flux',size=20)
+    plt.xlim(xmin,xmax)
+    plt.ylim(ymin,ymax)
+    plt.legend(fontsize=fs)
 
 #------------------------------------------------------------------------
 def HubbleDiagram(D,z,m='o',col='r'):
     
-    pylab.xlabel("Distance (Mpc)",size=20)
-    pylab.ylabel("Recession Velocity (km/s)",size=20)
-    pylab.plot(D,z,marker=m,c=col,ls='none',ms=10)
+    plt.xlabel("Distance (Mpc)",size=20)
+    plt.ylabel("Recession Velocity (km/s)",size=20)
+    plt.plot(D,z,marker=m,c=col,ls='none',ms=10)
     
 #------------------------------------------------------------------------
 def linfit(xdata, ydata, norm = 0, pinit=[70.0]):
